e0e82ec1ee0bd1a1a41bcbb00915facac83a00a2
Boris Li <liba8936@protonmail.com>
2023-10-07 00:20:13 +0500

    Lecture02 part01 complete.

--- 1_Lectures/Lecture02/ru/gb/jdk/lecture02/Main.java
+++ 1_Lectures/Lecture02/ru/gb/jdk/lecture02/Main.java
@@ -2,6 +2,6 @@ package ru.gb.jdk.lecture02;
 
 public class Main {
     public static void main(String[] args) {
-        new MainWindow();
+        new Window();
     }
 }
--- 1_Lectures/Lecture02/ru/gb/jdk/lecture02/MainCanvas.java
+++
@@ -1,6 +0,0 @@
-package ru.gb.jdk.lecture02;
-
-import javax.swing.*;
-
-public class MainCanvas extends JPanel {
-}
